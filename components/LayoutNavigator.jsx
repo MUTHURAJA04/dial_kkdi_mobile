dbb3b42ed2d18e73bafd6f1ac9136f05c2c4116d
MUTHURAJA04 <muthurajaasaithambi@gmail.com>
2025-07-31 15:24:12 +0530

    abdual

--- components/LayoutNavigator.jsx
+++ components/LayoutNavigator.jsx
@@ -1,19 +1,46 @@
-import { View, Text } from 'react-native'
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from '../screens/Home';
 import Profile from '../screens/Profile';
-
+import Landing from '../screens/Landing';
+import Layout from './Layout';
+import BusinessRegister from '../screens/BusinessRegister';
+import UserRegister from '../screens/UserRegister';
+import Login from '../screens/Login';
+import BusinessStep2 from '../screens/BusinessStep2';
+import BusinessStep3 from '../screens/BusinessStep3';
 
 const Stack = createNativeStackNavigator();
+
+const withLayout =
+  (Component, options = {}) =>
+  props => (
+    <Layout {...options}>
+      <Component {...props} />
+    </Layout>
+  );
 
 const LayoutNavigator = () => {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="Home" component={Home} />
-      <Stack.Screen name="Profile" component={Profile} />
-    </Stack.Navigator>
-  )
-}
+      {/* ❌ NO Layout for Landing */}
+      <Stack.Screen name="Landing" component={Landing} />
+      <Stack.Screen name="BusinessRegister" component={BusinessRegister} />
+      <Stack.Screen name="BusinessStep2" component={BusinessStep2} />
+      <Stack.Screen name="BusinessStep3" component={BusinessStep3} />
 
-export default LayoutNavigator
+
+
+      <Stack.Screen name="UserRegister" component={UserRegister} />
+      <Stack.Screen name="Login" component={Login} />
+
+      
+
+      {/* ✅ Layout applied here */}
+      <Stack.Screen name="Home" component={withLayout(Home)} />
+      <Stack.Screen name="Profile" component={withLayout(Profile)} />
+    </Stack.Navigator>
+  );
+};
+
+export default LayoutNavigator;
